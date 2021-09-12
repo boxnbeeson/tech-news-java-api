ced6e1a4fb43d09d88ef8937a3922b901a069b56
Jacob Beeson <beeson.jp@gmail.com>
2021-09-11 21:57:47 -0500

    Entity Models

---
+++ technews/src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,13 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int a = 5;
+        double b = 47.5;
+        double sum = b + a;
+        String c = "Example";
+        String d = "Example" + 5;
+        System.out.println(sum);
+        System.out.println(d);
+    }
+}
